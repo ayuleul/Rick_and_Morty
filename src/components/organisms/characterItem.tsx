930f739ab9cb0006b64a9977437add216aed03fd
Leul Ayalew <ayuleul1427@gmail.com>
2022-12-22 07:42:12 +0300

    ng: update origin title

--- src/components/organisms/characterItem.tsx
+++ src/components/organisms/characterItem.tsx
@@ -42,7 +42,7 @@ const CharacterCard: React.FC<ICharacterItemProp> = ({item}) => {
           {item.status}
         </Text>
         <Text variant="subtitle" mt="xs">
-          Location: {item.origin.name}
+          First seen: {item.origin.name}
         </Text>
       </Box>
     </Box>
